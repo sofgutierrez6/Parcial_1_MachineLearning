2dafcaad9aeff46ce36559586da5e81813cba6a5
DanielGuatibonza <50093531+DanielGuatibonza@users.noreply.github.com>
2020-03-19 09:03:22 -0500

    Codigo para extraer imagenes de video

---
+++ Codigo/toFrames.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 01:39:10 2020
+
+@author: Daniel Guatibonza
+"""
+
+
+import cv2
+vidcap = cv2.VideoCapture('SuperMarioBros.mp4')
+success,image = vidcap.read()
+count = 0
+while success:
+  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+  success,image = vidcap.read()
+  print('Read a new frame: ', success)
+  count += 1
